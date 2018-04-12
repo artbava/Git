43f6fde659adba5d307c26ab0159704e08c91c5f
artbava <artbava@gmail.com>
2018-04-12 12:03:27 -0300

    Added 1 file

---
+++ Learning C#/Bank_Account.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class BankAccount {
+        private double balance=0;
+        public void Deposit(double n) {
+            balance += n;
+        }
+        public void Withdraw(double n) {
+            balance -= n;
+        }
+        public double GetBalance() {
+            return balance;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount b = new BankAccount();
+            Console.WriteLine("Qual a operação que deseja realizar? - digite o número correspondente e pressione enter:");
+            Console.WriteLine("1- Deposit");
+            Console.WriteLine("2- Withdraw");
+            Console.WriteLine("3- Show Balance");
+            Console.WriteLine("4- Exit");
+            opt = Console.ReadLine();
+            switch ({opt})
+            {
+                case 1:
+                Console.WriteLine("Qual a quantia que será depositada?");
+                n = Console.ReadLine ();
+                b.Deposit(n);
+                break;
+                case 2:
+                Console.WriteLine("Qual a quantia que será sacada?");
+                n = Console.ReadLine();
+                b.Withdraw(n);
+                break;
+                case 3:
+                Console.WriteLine(b.GetBalance());
+                break;
+                case 4:
+                Console.WriteLine("Ok. Até mais!");
+                break;
+                default:
+                Console.WriteLine("Opção invalida.");
+                break;
+            }                                   
+        }
+    }
+}
